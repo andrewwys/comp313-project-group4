36dea7255352863bfd2d696fb408b5018e3ea100
Sze Man TANG <szesandyt123@gmail.com>
2023-12-08 05:05:02 -0500

    Update backend API

--- frontend/main.js
+++ frontend/main.js
@@ -67,7 +67,7 @@ document.querySelector('#upload-button').addEventListener('click', function() {
   const formData = new FormData();
   formData.append('file', file);
 
-  fetch('http://127.0.0.1:5000/get_csv', {
+  fetch('https://comp313-backend.onrender.com/get_csv', {
     method: 'POST',
     body: formData
   })
